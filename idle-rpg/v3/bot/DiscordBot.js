d03a182b3a02f48f77c37cb56b37a3fe35906b25
sunnyh <sizzlorox@protonmail.com>
2026-04-06 11:43:22 +0800

    fix(sentry): only initialize instrument.js if SENTRY_DSN is set

--- idle-rpg/v3/bot/DiscordBot.js
+++ idle-rpg/v3/bot/DiscordBot.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production' && process.env.SENTRY_DNS) {
   // eslint-disable-next-line global-require
   require('./instrument');
 }
